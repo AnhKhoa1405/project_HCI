19290264613b564269a6ca58346bcceb18eb74ab
AnhKhoa1405 <truonganhkhoa1405@gmail.com>
2021-09-27 11:35:09 +0700

    edit HomePage

--- App.js
+++ App.js
@@ -1,24 +1,12 @@
 import { StatusBar } from "expo-status-bar";
 import React from "react";
 import { StyleSheet, View, ScrollView } from "react-native";
-import AnotherSales from "./AnotherSales";
-import FooterHome from "./FooterHome";
-import HeadingHome from "./HeadingHome";
-import MyCarousel from "./MyCarousel";
-import MyCategory from "./MyCategory";
-import MyService from "./MyService";
+import Home from "./Home/Home";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <HeadingHome />
-      <ScrollView showsVerticalScrollIndicator={false}>
-        <MyCarousel />
-        <MyCategory />
-        <MyService />
-        <AnotherSales />
-      </ScrollView>
-      <FooterHome />
+      <Home />
     </View>
   );
 }
---
+++ Home/Home.js
@@ -0,0 +1,32 @@
+import React from "react";
+import { StyleSheet, View, ScrollView } from "react-native";
+import AnotherSales from "./detailHome/AnotherSales";
+import FooterHome from "./detailHome/FooterHome";
+import HeadingHome from "./detailHome/HeadingHome";
+import MyCarousel from "./detailHome/MyCarousel";
+import MyCategory from "./detailHome/MyCategory";
+import MyService from "./detailHome/MyService";
+
+export default function Home() {
+  return (
+    <View style={styles.container}>
+      <HeadingHome />
+      <ScrollView showsVerticalScrollIndicator={false}>
+        <MyCarousel />
+        <MyCategory />
+        <MyService />
+        <AnotherSales />
+      </ScrollView>
+      <FooterHome />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "flex-start",
+    justifyContent: "flex-start",
+  },
+});
